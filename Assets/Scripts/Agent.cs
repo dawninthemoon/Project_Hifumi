2629852187169f03f5d8e3c4a683f7a14e28aaa8
dawninthemoon <nuts1925@naver.com>
2023-08-09 22:45:20 +0900

    아군 공격 범위 표시

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -5,35 +5,63 @@ using UnityEngine;
 public class Agent : MonoBehaviour {
     [SerializeField] private float _moveSpeed = 1f;
     [SerializeField] private float _radius = 0.5f;
+    [SerializeField] private float _attackRadius = 0.7f;
+    [SerializeField] private float _attackDistance = 0.2f;
+    [SerializeField] private Vector2 _attackRectSize = Vector2.one;
     public float Radius {
         get { return _radius; }
+    }
+    public float AttackRadius {
+        get { return _attackRadius; }
     }
 
     public void Move(Agent target) {
         Vector3 dir = (target.transform.position - transform.position).normalized;
         Vector3 targetPosition = target.transform.position;
-        float offset = 0.2f;
+        float offset = 0.1f;
         targetPosition -= dir * (target.Radius * 2f + offset);
 
         float t = (Time.deltaTime * _moveSpeed) / Vector2.Distance(transform.position, targetPosition);
         transform.position = Vector3.Lerp(transform.position, targetPosition, t);
     }
 
-    private void Update() {
-        DrawCircle();
+    public void Attack(Agent target) {
+        Vector3 dir = (target.transform.position - transform.position).normalized;
+        Vector2 attackPosition = transform.position + dir * _attackDistance;
+
+        float radian = Mathf.Atan2(dir.y, dir.x);
+        DrawRect(attackPosition, _attackRectSize, radian + Mathf.PI * 0.5f, Color.blue);
     }
 
-    private void DrawCircle() {
+    private void Update() {
+        DrawCircle(_radius, Color.green);
+        DrawCircle(_attackRadius, Color.blue);
+    }
+
+    private void DrawRect(Vector2 center, Vector2 size, float radian, Color color) {
+        Vector2 dir1 = new Vector2(Mathf.Cos(radian), Mathf.Sin(radian)).normalized;
+        dir1 = dir1 * _attackRectSize.x;
+
+        Vector2 dir2 = new Vector2(Mathf.Cos(radian + Mathf.PI * 0.5f), Mathf.Sin(radian + Mathf.PI * 0.5f)).normalized;
+        dir2 = dir2 * _attackRectSize.y;
+
+        Debug.DrawLine(center + dir1 + dir2, center - dir1 + dir2, color);
+        Debug.DrawLine(center - dir1 + dir2, center - dir1 - dir2, color);
+        Debug.DrawLine(center - dir1 - dir2, center + dir1 - dir2, color);
+        Debug.DrawLine(center + dir1 - dir2, center + dir1 + dir2, color);
+    }
+
+    private void DrawCircle(float radius, Color color) {
         Vector2 position = (Vector2)transform.position;
-        Vector2 cur = position + _radius * new Vector2(Mathf.Cos(0f), Mathf.Sin(0f));
+        Vector2 cur = position + radius * new Vector2(Mathf.Cos(0f), Mathf.Sin(0f));
         Vector2 prev = cur;
         
         for (float theta = 0.1f; theta < Mathf.PI * 2f; theta += 0.1f) {
-            cur = position + _radius * new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
-            Debug.DrawLine(cur, prev, Color.green);
+            cur = position + radius * new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
+            Debug.DrawLine(cur, prev, color);
             prev = cur;
         }
-        cur = position + _radius * new Vector2(Mathf.Cos(0f), Mathf.Sin(0f));
-        Debug.DrawLine(cur, prev, Color.green);
+        cur = position + radius * new Vector2(Mathf.Cos(0f), Mathf.Sin(0f));
+        Debug.DrawLine(cur, prev, color);
     }
 }
--- Assets/Scripts/GameTest.cs
+++ Assets/Scripts/GameTest.cs
@@ -21,8 +21,8 @@ public class GameTest : MonoBehaviour {
         _allAgents = new List<Agent>();
 
         for (int i = 0; i < _entityCount; ++i) {
-            Vector3 randPos1 = new Vector3(Random.Range(0f, Width / 2f), Random.Range(0f, Height / 2f));
-            Vector3 randPos2 = new Vector3(Random.Range(0f, Width / 2f), Random.Range(0f, Height / 2f));
+            Vector3 randPos1 = new Vector3(Random.Range(-Width / 2f, Width / 2f), Random.Range(-Height / 2f, Height / 2f));
+            Vector3 randPos2 = new Vector3(Random.Range(-Width / 2f, Width / 2f), Random.Range(-Height / 2f, Height / 2f));
             
             var ally = Instantiate(_allyPrefab, randPos1, Quaternion.identity);
             var enemy = Instantiate(_enemyPrefab, randPos2, Quaternion.identity);
@@ -39,6 +39,11 @@ public class GameTest : MonoBehaviour {
     }
 
     private void Update() {
+        MoveProgress();
+        AttackProgress();
+    }
+
+    private void MoveProgress() {
         foreach (Agent ally in _allies) {
             Agent target = _enemies.FindClosest(ally.transform.position);
 
@@ -52,20 +57,34 @@ public class GameTest : MonoBehaviour {
         }
     }
 
+    private void AttackProgress() {
+        foreach (Agent ally in _allies) {
+            Agent target = _enemies.FindClosest(ally.transform.position);
+
+            if (IsIntersects(target.transform.position, ally.transform.position, target.AttackRadius, ally.Radius)) {
+                ally.Attack(target);
+            }
+        }
+    }
+
     private void EdgeOut(Agent agent, Agent other) {
         Vector3 dir = (other.transform.position - agent.transform.position).normalized;
         other.transform.position += dir * (agent.Radius * 0.1f);
         agent.transform.position -= dir * (agent.Radius * 0.1f);
     }
 
+    public bool IsIntersects(Vector2 p1, Vector2 p2, float r1, float r2) {
+        float sqrX = Mathf.Pow(p1.x - p2.x, 2f);
+        float sqrY = Mathf.Pow(p1.y - p2.y, 2f);
+        float sqrRadius = Mathf.Pow(r1 + r2, 2f);
+
+        return sqrX + sqrY < sqrRadius;
+    }
+
     public bool IsIntersects(Agent target, Agent other) {
         Vector2 p1 = target.transform.position;
         Vector2 p2 = other.transform.position;
 
-        float sqrX = Mathf.Pow(p1.x - p2.x, 2f);
-        float sqrY = Mathf.Pow(p1.y - p2.y, 2f);
-        float sqrRadius = Mathf.Pow(target.Radius + other.Radius, 2f);
-
-        return sqrX + sqrY < sqrRadius;
+        return IsIntersects(p1, p2, target.Radius, other.Radius);
     }
 }
