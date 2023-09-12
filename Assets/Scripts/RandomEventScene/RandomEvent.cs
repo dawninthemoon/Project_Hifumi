bf2981ad8beb1c159fc18e317392dab599e56498
dawninthemoon <nuts1925@naver.com>
2023-09-12 17:24:19 +0900

    프로젝트에 UniRx 추가

--- Assets/Scripts/RandomEventScene/RandomEvent.cs
+++ Assets/Scripts/RandomEventScene/RandomEvent.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class RandomEvent {
-    
+namespace RandomEvent {
+    public class Slaver : IRandomEvent {
+        public void Execute() {
+            
+        }
+    }
 }
