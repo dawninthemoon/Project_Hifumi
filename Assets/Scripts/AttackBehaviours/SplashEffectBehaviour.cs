ca3741e8ff7ccf3153639831e3adecc630661903
dawninthemoon <nuts1925@naver.com>
2023-09-22 21:20:51 +0900

    스플래시 이펙트에 주석처리

--- Assets/Scripts/AttackBehaviours/SplashEffectBehaviour.cs
+++ Assets/Scripts/AttackBehaviours/SplashEffectBehaviour.cs
@@ -6,12 +6,15 @@ namespace AttackBehaviours {
     [CreateAssetMenu(fileName = "SplashEffectBehaviour", menuName = "ScriptableObjects/AttackBehaviours/SplashEffect")]
     public class SplashEffectBehaviour : AttackBehaviour {
         public override void Behaviour(EntityBase caster, List<EntityBase> targets, Effects.IAttackEffect[] effects) {
+            // TODO: 이펙트 생기면 추가할 것
+            /*
             var sonarEffect = Resources.Load<ParticleSystem>("Fx/fxSonar");
             sonarEffect = Instantiate(sonarEffect, caster.transform.position, Quaternion.identity);
 
             foreach (Effects.IAttackEffect effect in effects) {
                 effect.ApplyEffect(caster, targets);
             }
+            */
         }
     }
 }
